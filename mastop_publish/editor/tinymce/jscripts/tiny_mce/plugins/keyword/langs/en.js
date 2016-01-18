2aa9f0c5358ac273739075d3b87429c0842a1fd7
mambax7 <mambax7@gmail.com>
2016-01-18 02:47:16 -0500

    Bring everything back up one level

--- mastop_publish/editor/tinymce/jscripts/tiny_mce/plugins/keyword/langs/en.js
+++
@@ -1,6 +0,0 @@
-// UK lang variables
-
-tinyMCE.addToLang('keyword',{
-title : 'Keyword',
-keyword_default : '-- Select keyword --'
-});
--- mastop_publish/editor/tinymce/jscripts/tiny_mce/plugins/keyword/langs/pt_br.js
+++
@@ -1,6 +0,0 @@
-// UK lang variables
-
-tinyMCE.addToLang('keyword',{
-title : 'Palavras-Chave',
-keyword_default : '-- Palavras-Chave --'
-});
